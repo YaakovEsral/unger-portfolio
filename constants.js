3cc81693761caa2e091ce3fdcaa60439eb054cca
Yaakov Esral <yaakovez@gmail.com>
2021-12-23 21:00:56 -0500

    added constants file

---
+++ constants.js
@@ -0,0 +1,13 @@
+const tempDirectory = './temp';
+const mobileCoverImage = 'mobile-cover';
+const desktopCoverImage = 'desktop-cover';
+const allowedImageTypes = ['.jpg', '.jpeg', '.png', '.gif'];
+const allowedVideoTypes = ['.mp4', '.mov'];
+
+module.exports = {
+    tempDirectory,
+    mobileCoverImage,
+    desktopCoverImage,
+    allowedImageTypes,
+    allowedVideoTypes
+}
