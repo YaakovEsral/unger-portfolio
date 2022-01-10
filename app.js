1fabf91d6acebed94c08d3c52881e776f5a41ddf
Yaakov Esral <yaakovez@gmail.com>
2022-01-09 20:35:25 -0500

    updated error handling

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')), express.static(path.join(__dirname, 'node_modules')) );
+app.use(express.static(path.join(__dirname, 'public')), express.static(path.join(__dirname, 'node_modules')));
 
 app.use((req, res, next) => {
     res.locals.path = req.path;
@@ -36,32 +36,36 @@ app.use('/portfolio', portfolioRouter);
 app.use('/admin', adminRouter)
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(function (req, res, next) {
+    next(createError(404));
 });
 
 
 // **** MIGHT NEED MORE TESTING
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
 
-    if(err.type === 'add-project-form') {
+    if (err.type === 'add-project-form') {
         res.statusCode = 400;
         return res.end(err.message);
     }
 
-  // set locals, only providing error in development
-//   console.log('message', err.message);
-//   console.log('error field', err.field);
-  res.locals.message = err.message;
-//   res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    //   console.log('message', err.message);
+    //   console.log('error field', err.field);
+    res.locals.message = err.message;
+    //   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
 
-// console.log(err.status);
+    // console.log(err.status);
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error', {error: err});
+    // render the error page
+    res.status(err.status || 500);
+
+    if (res.status === 404)
+        res.render('error', { error: err });
+    else
+        res.end(err.message || 'Unknown error occurred. Please contact your administrator.')
 });
 
 module.exports = app;
