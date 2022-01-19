0782674abc19e1114a68e104023102e3317db7b3
Yaakov Esral <yaakovez@gmail.com>
2022-01-19 16:17:26 -0500

    added new utils for fade effect

--- public/javascripts/utils.js
+++ public/javascripts/utils.js
@@ -6,6 +6,14 @@ function hide(elem) {
     elem.classList.add('hidden');
 }
 
+function fadeShow(elem) {
+    elem.classList.remove('hidden-fade');
+}
+
+function fadeHide(elem) {
+    elem.classList.add('hidden-fade');
+}
+
 function toggleShowHide(elem) {
     if (elem.classList.includes('hidden')) {
         elem.classList.remove('hidden');
