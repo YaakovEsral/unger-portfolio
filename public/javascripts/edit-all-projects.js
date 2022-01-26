c0797bad68a5f8e7fb04e7ba40437f70ab61dfef
Yaakov Esral <yaakovez@gmail.com>
2022-01-26 16:01:49 -0500

    relocated delete project function to fetch-utils file

--- public/javascripts/edit-all-projects.js
+++ public/javascripts/edit-all-projects.js
@@ -3,22 +3,5 @@ const showDeleteDialogButtons = document.querySelectorAll('#show-delete-project-
 
 // When any of the 'Delete this project' buttons are clicked, show dialog to delete
 showDeleteDialogButtons.forEach(button => {
-    button.addEventListener('click', showDeleteDialog)
+    button.addEventListener('click', adminShowDialog.deleteProject)
 })
-
-// Function to show the delete dialog
-function showDeleteDialog(event) {
-    // console.log(event);
-    // Show delete popup
-    deleteProjectDialog.classList.remove('hidden');
-    // Set inner content
-    deleteProjectDialog.innerHTML = popupHTML.deleteProject();
-    // Add listener to hide dialog when "cancel" is clicked
-    get('delete-project-cancel').addEventListener('click', () => hide(deleteProjectDialog))
-    // Add listener to delete the identified project when the final delete button is clicked
-    get('delete-project-button').addEventListener('click', async () => {
-        const response = await deleteProject(event);
-        deleteProjectDialog.innerHTML = `<p class=${response.responseClass} id="delete-project-response">${response.message}</p>`;
-        setTimeout(() => hide(deleteProjectDialog), response.timeout)
-    })
-}
