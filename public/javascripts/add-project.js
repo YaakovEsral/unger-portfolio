60ce982520bc9827cd12ff67862857b315967183
Yaakov Esral <yaakovez@gmail.com>
2022-01-26 15:58:01 -0500

    cleaned file by relocating variables and functions

--- public/javascripts/add-project.js
+++ public/javascripts/add-project.js
@@ -1,109 +1,9 @@
-function get(id) {
-    return document.getElementById(id);
-}
+/* 
+Variables are defined in admin-add-project-variables.js.
+Functions are in handle-admin-inputs.js
 
-function handleTextInput(event) {
-    // console.log(event.target.value);
-    return event.target.value;
-}
-
-function handleRadioInput(event) {
-    // console.log(event.target.value);
-    return event.target.value;
-}
-
-// This handler is can only handle credits key-value pairs
-function handleKeyValueInput() {
-    const credits = {};
-
-    for (let i = 0; i < allCreditsContainer.children.length; i++) {
-        // save the values of the two inputs in each single-credit div
-        const key = allCreditsContainer.children[i].children[1].value;
-        const value = allCreditsContainer.children[i].children[2].value;
-        // console.log(key, value);
-
-        // save them only if they are valid values
-        if (key && value)
-            credits[key] = value;
-    }
-
-    // console.log(formValues, formValues.credits);
-
-    return JSON.stringify(credits);
-}
-
-function handleMediaInput(event) {
-    // console.log(event);
-    // console.log(event.target.parentElement.children[1]);
-    const previewImage = event.target.parentElement.children[1];
-    // const previewImage = event.target.parentElement.children[/preview/]); // regex for containing "preview" in ID
-    const url = URL.createObjectURL(event.target.files[0]);
-    previewImage.src = url;
-}
-
-function setDefaultSlug(event) {
-    // console.log(event.target.value);
-    const input = event.target.value;
-    const slug = input.toLowerCase().replace(/\s/g, '-').replace(/\W^-/g, '');
-    // console.log(slug);
-    const defaultSlugElem = document.querySelector('#default-slug span');
-    defaultSlugElem.innerText = slug;
-    return slug;
-}
-
-function showSubmitDialog(error, slug) {
-    popupBox.classList.remove('hidden');
-    // still need to test projectSubmitted
-    const htmlContent = error ? popupHTML.error(error) : popupHTML.projectSubmitted(slug);
-    popupBox.innerHTML = htmlContent;
-}
-
-function showDeleteDialog(slug, file, event) {
-    popupBox.classList.remove('hidden');
-    // console.log(slug, file)
-    popupBox.innerHTML = popupHTML.deleteImage();
-    get('delete-image-button').addEventListener('click', () => deleteImage(slug, file, event))
-}
-
-// Note that projectType and showOnHomePage are set automatically in db if they are left null.
-// Project type defaults to branding and showOnHomePage defaults to 0 (false);
-
-// The form
-const addProjectForm = get('add-project-form');
-
-// Elements
-const projectNameElem = get('input-project-name');          // text
-const slugElem = get('input-project-slug');                 // text
-const projectTypeDiv = get('project-type-div');             // radio
-const clientNameElem = get('input-client-name');            // text
-const creditsDivElem = get('credits-div');                  // key value pairs
-const projectDescriptionElem = get('input-project-description');   // text
-const dateCompletedElem = get('input-date-completed');      // text
-const showOnHomePageDiv = get('show-on-homepage-div');        // radio
-
-const allCreditsContainer = get('all-single-credits-container');// container
-const moreCreditsInput = get('more-credits-input');          // number input
-const allDesktopInsideMediaContainer = get('all-desktop-inside-media-container');// container
-const allMobileInsideMediaContainer = get('all-mobile-inside-media-container');// container
-const moreDesktopMediaInput = get('more-desktop-media-input');             // number input
-const moreMobileMediaInput = get('more-mobile-media-input');             // number input
-
-// Media input
-const desktopPhotoInput = get('desktop-cover-photo-input');
-const mobilePhotoInput = get('mobile-cover-photo-input');
-const desktopInsideMediaDiv = get('desktop-inside-media-div');
-const mobileInsideMediaDiv = get('mobile-inside-media-div');
-
-// Buttons
-const moreCreditsButton = get('more-credits-button');
-const moreDesktopMediaButton = get('more-desktop-media-button');
-const moreMobileMediaButton = get('more-mobile-media-button');
-
-// Popup
-const popupBox = get('form-popup');
-
-// Misc.
-var defaultSlug = '';
+This file puts everything together - adding listeners for change and submit events.
+*/
 
 // Starting form values
 const formValues = {
@@ -118,8 +18,20 @@ const formValues = {
     projectID: get('project-id').value
 }
 
-console.log(formValues);
-// setInterval(()=> console.log(formValues), 10000);
+function setDefaultSlug(event) {
+    // console.log(event.target.value);
+    const input = event.target.value;
+    const slug = input.toLowerCase().replace(/\s/g, '-').replace(/\W^-/g, '');
+    // console.log(slug);
+    const defaultSlugElem = document.querySelector('#default-slug span');
+    defaultSlugElem.innerText = slug;
+    return slug;
+}
+
+// Note that projectType and showOnHomePage are set automatically in db if they are left null.
+// Project type defaults to branding and showOnHomePage defaults to 0 (false);
+
+// console.log(formValues);
 
 // listeners for text values
 projectNameElem.addEventListener('change', e => {
@@ -144,22 +56,14 @@ creditsDivElem.addEventListener('change', () => formValues.credits = handleKeyVa
 // listeners for media inputs
 desktopPhotoInput.addEventListener('change', e => handleMediaInput(e));
 mobilePhotoInput.addEventListener('change', e => handleMediaInput(e));
-desktopInsideMediaDiv.addEventListener('change', e => handleMediaInput(e));
-mobileInsideMediaDiv.addEventListener('change', e => handleMediaInput(e));
+allDesktopInsideMediaContainer.addEventListener('change', e => handleMediaInput(e));
+allMobileInsideMediaContainer.addEventListener('change', e => handleMediaInput(e));
 
 // adding inputs for credits
 moreCreditsButton.addEventListener('click', (e) => {
     // Get the value of moreCreditsInput and append that many single-credits divs
     for (let i = 0; i < +moreCreditsInput.value; i++) {
-        allCreditsContainer.insertAdjacentHTML('beforeend',
-            `
-        <div class="single-credits">
-            <span class="credits-delete">&#10006</span>
-            <input type="text" name="credit-type" placeholder="Credit Type" />
-            <input type="text" name="credit-name" placeholder="Name" />
-        </div>
-        `
-        );
+        allCreditsContainer.insertAdjacentHTML('beforeend', htmlSnippets.singleCredits());
     }
 })
 
@@ -174,15 +78,7 @@ allCreditsContainer.addEventListener('click', e => {
 moreDesktopMediaButton.addEventListener('click', (e) => {
     // Get the value of moreDesktopMediaInput and append that many single-desktop-inside-media divs
     for (let i = 0; i < +moreDesktopMediaInput.value; i++) {
-        allDesktopInsideMediaContainer.insertAdjacentHTML('beforeend',
-            `
-        <div class="single-desktop-inside-media">
-            <span class="inside-media-delete">&#10006</span>
-            <img class="inside-media-preview preview-image" src="/images/no-image.png">
-            <input class="desktop-inside-media-input" type="file" name="single-desktop-inside-media" />
-        </div>
-        `
-        );
+        allDesktopInsideMediaContainer.insertAdjacentHTML('beforeend', htmlSnippets.singleDesktopInsideMedia());
     }
 })
 
@@ -190,7 +86,7 @@ moreDesktopMediaButton.addEventListener('click', (e) => {
 allDesktopInsideMediaContainer.addEventListener('click', e => {
     if(e.target.dataset.file) {
         // console.log(e.target.dataset.file)
-        showDeleteDialog(addProjectForm.dataset.slug, e.target.dataset.file, e);
+        adminShowDialog.deleteImage(addProjectForm.dataset.slug, e.target.dataset.file, e);
         // code to create "Delete File" dialog and then delete file and remove element
     }
     else if (e.target.classList.contains('inside-media-delete')) {
@@ -202,15 +98,7 @@ allDesktopInsideMediaContainer.addEventListener('click', e => {
 moreMobileMediaButton.addEventListener('click', (e) => {
     // Get the value of moreMobileMediaInput and append that many single-mobile-inside-media divs
     for (let i = 0; i < +moreMobileMediaInput.value; i++) {
-        allMobileInsideMediaContainer.insertAdjacentHTML('beforeend',
-            `
-        <div class="single-mobile-inside-media">
-            <span class="inside-media-delete">&#10006</span>
-            <img class="inside-media-preview preview-image" src="/images/no-image.png">
-            <input class="mobile-inside-media-input" type="file" name="single-mobile-inside-media" />
-        </div>
-        `
-        );
+        allMobileInsideMediaContainer.insertAdjacentHTML('beforeend', htmlSnippets.singleMobileInsideMedia());
     }
 })
 
@@ -218,7 +106,7 @@ moreMobileMediaButton.addEventListener('click', (e) => {
 allMobileInsideMediaContainer.addEventListener('click', e => {
     if(e.target.dataset.file) {
         // console.log(e.target.dataset.file)
-        showDeleteDialog(addProjectForm.dataset.slug, e.target.dataset.file, e);
+        adminShowDialog.deleteImage(addProjectForm.dataset.slug, e.target.dataset.file, e);
         // code to create "Delete File" dialog and then delete file and remove element
     }
     else if (e.target.classList.contains('inside-media-delete')) {
@@ -247,38 +135,8 @@ addProjectForm.addEventListener('submit', async (e) => {
     //     console.log('fd', value);
     //  }
 
-    let delayTime;
-    let response;
-    try {
-        // Note that default format is multipart
-        response = await fetch('/admin/add-project', {
-            method: 'POST',
-            body: formData
-        });
-        if (!response.ok) {
-            const message = await response.text();
-            throw new Error(message || response.statusText);
-        }
-        delayTime = 5000;
-        const parsedResponse = await response.json();
-        console.log(response, parsedResponse);
-        showSubmitDialog(false, parsedResponse.slug);
-    } catch (err) {
-        delayTime = 3000;
-        showSubmitDialog(err)
-        console.error(err);
-        return err;
-    }
-    finally {
-        setTimeout(() => {
-            popupBox.classList.add('hidden')
-            if (response.ok) {
-                window.location.assign('/admin/dashboard')
-            }
-        }, delayTime);
-
-    }
-
+    // Note that admin dialog also executes the fetch call
+    adminShowDialog.submitProject(formData);
 })
 
 // listener to remove popup on click
