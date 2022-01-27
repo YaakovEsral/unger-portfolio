e3e4cb6136269682bbc7219a382ed556466fcb4e
Yaakov Esral <yaakovez@gmail.com>
2022-01-27 14:06:01 -0500

    changed vars to const

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 require('dotenv').config();
 const flash = require('express-flash');
 const session = require('express-session');
@@ -10,12 +10,11 @@ const passport = require('passport');
 const { checkAuthenticated } = require('./middleware/authentication')
 const methodOverride = require('method-override');
 
-var indexRouter = require('./routes/index');
+const indexRouter = require('./routes/index');
 const portfolioRouter = require('./routes/portfolio');
 const adminRouter = require('./routes/admin');
 
-var app = express();
-
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -48,7 +47,7 @@ app.use((req, res, next) => {
 
 app.use('/', indexRouter);
 app.use('/portfolio', portfolioRouter);
-app.use('/admin', checkAuthenticated, adminRouter)
+app.use('/admin', checkAuthenticated, adminRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
