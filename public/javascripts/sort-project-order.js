f7b6c6a06fd252d5799e21b976e7929d63828650
Yaakov Esral <yaakovez@gmail.com>
2022-02-03 19:39:35 -0500

    changed name to sort-project-order

---
+++ public/javascripts/sort-project-order.js
@@ -0,0 +1,29 @@
+const element = document.getElementById('all-projects-container');
+const sortable = Sortable.create(element, { onSort, dragoverBubble: true });
+
+async function onSort(e) {
+    // console.log(e);
+    const data = {
+        newIndex: e.newIndex,
+        oldIndex: e.oldIndex
+    }
+    try {
+        const response = await fetch('/admin/update-project-order', {
+            method: 'post',
+            body: JSON.stringify(data),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+
+        if (!response.ok) {
+            const message = await response.text();
+            throw new Error(`${response.status} - ${message || response.statusText}`)
+        }
+        return response;
+    }
+    catch (err) {
+        console.error(err);
+        return response;
+    }
+}
