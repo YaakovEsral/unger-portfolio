a40e75e20d49310ab0896319c30a998a93e130be
Yaakov Esral <yaakovez@gmail.com>
2022-02-07 18:07:38 -0500

    popup clicking and click away behavior

--- public/javascripts/credits-popup.js
+++ public/javascripts/credits-popup.js
@@ -1,15 +1,26 @@
 const creditsButton = get('project-credits-button');
 const creditsPopup = get('project-credits-popup');
+const exitIcon = get('credits-exit-icon');
 
 creditsButton.addEventListener('click', (e) => {
-    if(e.target===creditsButton)
+    if (!creditsPopup.classList.contains('hidden-fade') && !e.path.includes(creditsPopup))
+        return fadeHide(creditsPopup);
+
+    if (e.target === creditsButton)
         // show(creditsPopup)
         fadeShow(creditsPopup);
     // console.log(e.target);
 });
 
-creditsPopup.addEventListener('click', () => {
+exitIcon.addEventListener('click', () => {
     // console.log('clicked');
     // hide(creditsPopup) 
     fadeHide(creditsPopup);
 });
+
+document.body.addEventListener('click', e => {
+
+    if (!e.path.includes(creditsButton) && !e.path.includes(creditsPopup)) {
+        fadeHide(creditsPopup)
+    }
+})
